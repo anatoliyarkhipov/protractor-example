8a333daaa56c0fdd82c44e360caf733dab5d7343
aarkhipov <aarkhipov1991@gmail.com>
2017-11-08 19:42:37 +0400

    add no-promise version

--- tests/conf.js
+++ tests/conf.js
@@ -2,7 +2,7 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['lol.test.js'],
 
-  SELENIUM_PROMISE_MANAGER: false,
+  // SELENIUM_PROMISE_MANAGER: false,
 
   onPrepare: function() {
     browser.ignoreSynchronization = true
--- tests/lol.test.js
+++ tests/lol.test.js
@@ -1,16 +1,17 @@
 describe('Please, click on the link', function() {
   it('should just click on the link', function() {
+    browser.get('http://example.com');
 
     const element = protractor.element(by.css('a[href="http://www.iana.org/domains/example"]'))
 
-    return Promise.resolve()
-      .then(function() { return browser.get('http://example.com'); })
-      .then(function() { return browser.sleep(1000); })
-      .then(function() { return console.log('before move'); })
-      .then(function() { return browser.actions().mouseMove(element).perform(); })
-      .then(function() { return console.log('before click'); })
-      .then(function() { return browser.actions().click(element, protractor.Button.LEFT).perform(); })
-      .then(function() { return console.log('after click'); })
-      .then(function() { return browser.sleep(3000); })
+    browser.sleep(1000)
+
+    console.log('before move')
+    browser.actions().mouseMove(element).perform();
+    console.log('before click')
+    browser.actions().click(element, protractor.Button.LEFT).perform();
+    console.log('after click')
+
+    browser.sleep(3000)
   });
 });
